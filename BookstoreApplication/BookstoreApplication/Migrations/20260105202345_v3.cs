ad566d15b29665e49876c6908171d636a111fbaa
Jelena Stanojoski <stanojoskijelena997@gmail.com>
2026-01-05 21:26:47 +0100

    Implementirani testni podaci.

---
+++ BookstoreApplication/BookstoreApplication/Migrations/20260105202345_v3.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BookstoreApplication.Migrations
+{
+    /// <inheritdoc />
+    public partial class v3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PublishedDate",
+                table: "Books",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Birthday",
+                table: "Author",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.InsertData(
+                table: "Author",
+                columns: new[] { "Id", "Biography", "Birthday", "FullName" },
+                values: new object[,]
+                {
+                    { 1, "British author", new DateTime(1965, 7, 31, 0, 0, 0, 0, DateTimeKind.Unspecified), "J.K. Rowling" },
+                    { 2, "American novelist", new DateTime(1948, 9, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), "George R.R. Martin" },
+                    { 3, "English writer", new DateTime(1890, 9, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), "Agatha Christie" },
+                    { 4, "British writer", new DateTime(1892, 1, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), "J.R.R. Tolkien" },
+                    { 5, "American author", new DateTime(1947, 9, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), "Stephen King" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Awards",
+                columns: new[] { "Id", "Description", "Name", "Year" },
+                values: new object[,]
+                {
+                    { 1, "Top fiction award", "Best Fiction", 2000 },
+                    { 2, "Voted by readers", "Reader's Choice", 2005 },
+                    { 3, "Lifetime award", "Lifetime Achievement", 2010 },
+                    { 4, "Award for best novel", "Best Novel", 2020 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Publisher",
+                columns: new[] { "Id", "Address", "Name", "Website" },
+                values: new object[,]
+                {
+                    { 1, "London", "Penguin Books", "www.penguin.com" },
+                    { 2, "New York", "HarperCollins", "www.harpercollins.com" },
+                    { 3, "New York", "Random House", "www.randomhouse.com" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AuthorAwardBridge",
+                columns: new[] { "Id", "AuthorId", "AwardId", "YearAward" },
+                values: new object[,]
+                {
+                    { 1, 1, 1, 2000 },
+                    { 2, 1, 2, 2005 },
+                    { 3, 2, 1, 2000 },
+                    { 4, 2, 4, 2020 },
+                    { 5, 3, 3, 2010 },
+                    { 6, 3, 2, 2005 },
+                    { 7, 4, 4, 2020 },
+                    { 8, 4, 1, 2000 },
+                    { 9, 5, 2, 2005 },
+                    { 10, 5, 4, 2020 },
+                    { 11, 1, 4, 2020 },
+                    { 12, 2, 2, 2005 },
+                    { 13, 3, 1, 2000 },
+                    { 14, 4, 2, 2005 },
+                    { 15, 5, 3, 2010 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "AuthorId", "ISBN", "PageCount", "PublishedDate", "PublisherId", "Title" },
+                values: new object[,]
+                {
+                    { 1, 1, "9780747532699", 320, new DateTime(1997, 6, 26, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "Harry Potter 1" },
+                    { 2, 1, "9780747538493", 341, new DateTime(1998, 7, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "Harry Potter 2" },
+                    { 3, 2, "9780553103540", 694, new DateTime(1996, 8, 6, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, "A Game of Thrones" },
+                    { 4, 2, "9780553108033", 768, new DateTime(1998, 11, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, "A Clash of Kings" },
+                    { 5, 3, "9780007119318", 256, new DateTime(1934, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 3, "Murder on the Orient Express" },
+                    { 6, 3, "9780062073488", 272, new DateTime(1939, 11, 6, 0, 0, 0, 0, DateTimeKind.Unspecified), 3, "And Then There Were None" },
+                    { 7, 4, "9780547928227", 310, new DateTime(1937, 9, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "The Hobbit" },
+                    { 8, 4, "9780544003415", 1178, new DateTime(1954, 7, 29, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "The Lord of the Rings" },
+                    { 9, 5, "9780385121675", 447, new DateTime(1977, 1, 28, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, "The Shining" },
+                    { 10, 5, "9781501142970", 1138, new DateTime(1986, 9, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, "It" },
+                    { 11, 5, "9780385086953", 199, new DateTime(1974, 4, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), 3, "Carrie" },
+                    { 12, 1, "9780439321600", 128, new DateTime(2001, 11, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 3, "Fantastic Beasts" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumn: "Id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Author",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Author",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Author",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Author",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Author",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Awards",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Awards",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Awards",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Awards",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Publisher",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Publisher",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Publisher",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PublishedDate",
+                table: "Books",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Birthday",
+                table: "Author",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+        }
+    }
+}
--- BookstoreApplication/BookstoreApplication/Models/AppDbContext.cs
+++ BookstoreApplication/BookstoreApplication/Models/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace BookstoreApplication.Models
 {
@@ -14,23 +15,21 @@ namespace BookstoreApplication.Models
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<AuthorAward>(entity =>
             {
                 entity.ToTable("AuthorAwardBridge");
-                
+                entity.HasOne(a => a.Author)
+                      .WithMany(aa => aa.AuthorAwards)
+                      .HasForeignKey(a => a.AuthorId)
+                      .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasOne(a => a.Award)
+                      .WithMany(aa => aa.AuthorAwards)
+                      .HasForeignKey(a => a.AwardId)
+                      .OnDelete(DeleteBehavior.Cascade);
             });
-
-            modelBuilder.Entity<AuthorAward>()
-                .HasOne(a => a.Author)
-                .WithMany(aa => aa.AuthorAwards)
-                .HasForeignKey(a => a.AuthorId)
-                .OnDelete(DeleteBehavior.Cascade);
-
-            modelBuilder.Entity<AuthorAward>()
-               .HasOne(a => a.Award)
-               .WithMany(aa => aa.AuthorAwards)
-               .HasForeignKey(a => a.AwardId)
-               .OnDelete(DeleteBehavior.Cascade);
 
             modelBuilder.Entity<Book>()
                 .HasOne(b => b.Publisher)
@@ -38,10 +37,70 @@ namespace BookstoreApplication.Models
                 .HasForeignKey(k => k.PublisherId)
                 .OnDelete(DeleteBehavior.Restrict);
 
-            modelBuilder.Entity<Author>(enetity =>
-            {
-                enetity.Property(a => a.DateOfBirth).HasColumnName("Birthday");
-            });
+            modelBuilder.Entity<Author>()
+                .Property(a => a.DateOfBirth)
+                .HasColumnName("Birthday")
+                .HasColumnType("timestamp without time zone");
+
+            modelBuilder.Entity<Book>()
+                .Property(b => b.PublishedDate)
+                .HasColumnType("timestamp without time zone");
+
+            // TESTNI PODACI
+
+            modelBuilder.Entity<Author>().HasData(
+                new Author { Id = 1, FullName = "J.K. Rowling", Biography = "British author", DateOfBirth = new DateTime(1965, 7, 31) },
+                new Author { Id = 2, FullName = "George R.R. Martin", Biography = "American novelist", DateOfBirth = new DateTime(1948, 9, 20) },
+                new Author { Id = 3, FullName = "Agatha Christie", Biography = "English writer", DateOfBirth = new DateTime(1890, 9, 15) },
+                new Author { Id = 4, FullName = "J.R.R. Tolkien", Biography = "British writer", DateOfBirth = new DateTime(1892, 1, 3) },
+                new Author { Id = 5, FullName = "Stephen King", Biography = "American author", DateOfBirth = new DateTime(1947, 9, 21) }
+            );
+
+            modelBuilder.Entity<Publisher>().HasData(
+                new Publisher { Id = 1, Name = "Penguin Books", Address = "London", Website = "www.penguin.com" },
+                new Publisher { Id = 2, Name = "HarperCollins", Address = "New York", Website = "www.harpercollins.com" },
+                new Publisher { Id = 3, Name = "Random House", Address = "New York", Website = "www.randomhouse.com" }
+            );
+
+            modelBuilder.Entity<Award>().HasData(
+                new Award { Id = 1, Name = "Best Fiction", Description = "Top fiction award", Year = 2000 },
+                new Award { Id = 2, Name = "Reader's Choice", Description = "Voted by readers", Year = 2005 },
+                new Award { Id = 3, Name = "Lifetime Achievement", Description = "Lifetime award", Year = 2010 },
+                new Award { Id = 4, Name = "Best Novel", Description = "Award for best novel", Year = 2020 }
+            );
+
+            modelBuilder.Entity<Book>().HasData(
+                new Book { Id = 1, Title = "Harry Potter 1", AuthorId = 1, PublisherId = 1, ISBN = "9780747532699", PageCount = 320, PublishedDate = new DateTime(1997, 6, 26) },
+                new Book { Id = 2, Title = "Harry Potter 2", AuthorId = 1, PublisherId = 1, ISBN = "9780747538493", PageCount = 341, PublishedDate = new DateTime(1998, 7, 2) },
+                new Book { Id = 3, Title = "A Game of Thrones", AuthorId = 2, PublisherId = 2, ISBN = "9780553103540", PageCount = 694, PublishedDate = new DateTime(1996, 8, 6) },
+                new Book { Id = 4, Title = "A Clash of Kings", AuthorId = 2, PublisherId = 2, ISBN = "9780553108033", PageCount = 768, PublishedDate = new DateTime(1998, 11, 16) },
+                new Book { Id = 5, Title = "Murder on the Orient Express", AuthorId = 3, PublisherId = 3, ISBN = "9780007119318", PageCount = 256, PublishedDate = new DateTime(1934, 1, 1) },
+                new Book { Id = 6, Title = "And Then There Were None", AuthorId = 3, PublisherId = 3, ISBN = "9780062073488", PageCount = 272, PublishedDate = new DateTime(1939, 11, 6) },
+                new Book { Id = 7, Title = "The Hobbit", AuthorId = 4, PublisherId = 1, ISBN = "9780547928227", PageCount = 310, PublishedDate = new DateTime(1937, 9, 21) },
+                new Book { Id = 8, Title = "The Lord of the Rings", AuthorId = 4, PublisherId = 1, ISBN = "9780544003415", PageCount = 1178, PublishedDate = new DateTime(1954, 7, 29) },
+                new Book { Id = 9, Title = "The Shining", AuthorId = 5, PublisherId = 2, ISBN = "9780385121675", PageCount = 447, PublishedDate = new DateTime(1977, 1, 28) },
+                new Book { Id = 10, Title = "It", AuthorId = 5, PublisherId = 2, ISBN = "9781501142970", PageCount = 1138, PublishedDate = new DateTime(1986, 9, 15) },
+                new Book { Id = 11, Title = "Carrie", AuthorId = 5, PublisherId = 3, ISBN = "9780385086953", PageCount = 199, PublishedDate = new DateTime(1974, 4, 5) },
+                new Book { Id = 12, Title = "Fantastic Beasts", AuthorId = 1, PublisherId = 3, ISBN = "9780439321600", PageCount = 128, PublishedDate = new DateTime(2001, 11, 1) }
+            );
+
+            modelBuilder.Entity<AuthorAward>().HasData(
+                new AuthorAward { Id = 1, AuthorId = 1, AwardId = 1, YearAward = 2000 },
+                new AuthorAward { Id = 2, AuthorId = 1, AwardId = 2, YearAward = 2005 },
+                new AuthorAward { Id = 3, AuthorId = 2, AwardId = 1, YearAward = 2000 },
+                new AuthorAward { Id = 4, AuthorId = 2, AwardId = 4, YearAward = 2020 },
+                new AuthorAward { Id = 5, AuthorId = 3, AwardId = 3, YearAward = 2010 },
+                new AuthorAward { Id = 6, AuthorId = 3, AwardId = 2, YearAward = 2005 },
+                new AuthorAward { Id = 7, AuthorId = 4, AwardId = 4, YearAward = 2020 },
+                new AuthorAward { Id = 8, AuthorId = 4, AwardId = 1, YearAward = 2000 },
+                new AuthorAward { Id = 9, AuthorId = 5, AwardId = 2, YearAward = 2005 },
+                new AuthorAward { Id = 10, AuthorId = 5, AwardId = 4, YearAward = 2020 },
+                new AuthorAward { Id = 11, AuthorId = 1, AwardId = 4, YearAward = 2020 },
+                new AuthorAward { Id = 12, AuthorId = 2, AwardId = 2, YearAward = 2005 },
+                new AuthorAward { Id = 13, AuthorId = 3, AwardId = 1, YearAward = 2000 },
+                new AuthorAward { Id = 14, AuthorId = 4, AwardId = 2, YearAward = 2005 },
+                new AuthorAward { Id = 15, AuthorId = 5, AwardId = 3, YearAward = 2010 }
+            );
         }
     }
 }
